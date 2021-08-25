8686f2a6f8f8513283a2cadf0e4040d9b288316d
Garrison Parrish <tpgarris@live.unc.edu>
2021-08-24 22:26:35 -0400

    Fixed typo

--- rref-calculator.py
+++ rref-calculator.py
@@ -1,6 +1,5 @@
 """This program will convert a matrix to row-reduced echelon form."""
 
 def matrix_input():
-    """Prompts the user for dimensions of the matrix aand value in each cell."""
+    """Prompts the user for dimensions of the matrix and value in each cell."""
 
-    

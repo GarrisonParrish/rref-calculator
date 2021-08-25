308b954d078eda8f16a93e7c0ef70f493943e7bc
Garrison Parrish <tpgarris@live.unc.edu>
2021-08-24 22:17:18 -0400

    first commmit

---
+++ rref-calculator.py
@@ -0,0 +1,6 @@
+"""This program will convert a matrix to row-reduced echelon form."""
+
+def matrix_input():
+    """Prompts the user for dimensions of the matrix aand value in each cell."""
+
+    
